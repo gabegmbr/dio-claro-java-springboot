7f8687fd70ff47af30627b437e0e0612b1b963f3
gabegmbr <221022560@aluno.unb.br>
2024-08-25 16:35:32 -0300

    Commit inicial

---
+++ sintaxe-java/java-anatomia-classes/src/edu/gabriel/primeirasemana/BoletimEstudantil.java
@@ -0,0 +1,9 @@
+package edu.gabriel.primeirasemana;
+public class BoletimEstudantil {
+    public static void main(String[] args) {
+        int mediaFinal = 6;
+        if (mediaFinal < 6) System.out.print("Reprovado");
+        else if (mediaFinal == 6) System.out.println("Prova Minerva");
+        else System.out.println("Aprovado");
+    }
+}
---
+++ sintaxe-java/java-anatomia-classes/src/edu/gabriel/segundasemana/MyClass.java
@@ -0,0 +1,9 @@
+package edu.gabriel.segundasemana;
+public class MyClass {
+    public static void main(String[] args) {
+        System.out.print(nomeCompleto("Gabriel", "Maciel"));
+    }
+    public static String nomeCompleto (String nome, String sobrenome){
+        return nome + " " + sobrenome;
+    }
+}
