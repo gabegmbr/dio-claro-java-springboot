fd5d10fa64d911bc363e266cc70cdfecccef1d8a
gabegmbr <221022560@aluno.unb.br>
2024-09-27 19:35:02 -0300

    Abstraindo um Bootcamp Usando Orientação a Objetos em Java

---
+++ Desafio-POO/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Curso curso = new Curso();
+        System.out.println(curso);
+    }
+}
---
+++ Desafio-POO/src/Mentoria.java
@@ -0,0 +1,27 @@
+import java.time.LocalDate;
+
+public class Mentoria extends Conteudo {
+    LocalDate data;
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    @Override
+    public String toString() {
+        return "Mentoria{" +
+                "titulo='" + getTitulo() + '\'' +
+                ", descricao='" + getDescricao() + '\'' +
+                ", data=" + data +
+                '}';
+    }
+
+    @Override
+    public double calcularXP() {
+        return XP_PADRAO+20d;
+    }
+}
